31d96693e357ba5479bba3741edcdbfa067ed0c2
tg00021 <tg00021@localhost.localdomain>
2020-02-10 15:18:33 +0100

    Eigene Fehlermeldung falls dir nicht angelegt werden kann

--- backup.sh
+++ backup.sh
@@ -8,8 +8,8 @@ log="$logdir/Backup-Log_$current_date"
 errorlog="$logdir/Bakup-Fehler_$current_date"
 
 # nötige Directories anlegen
-mkdir -p "$backupdir"
-mkdir -p "$logdir"
+mkdir -p "$backupdir" 2>> /dev/null || echo "Konnte Verzeichnis $backupdir nicht anlegen"
+mkdir -p "$logdir" 2>> /dev/null || echo "Konnte Verzeichnis $logdir nicht anlegen"
 
 # backups anfertigen
 cp -av ~ "$backupdir" >> "$log" 2>> "$errorlog"
