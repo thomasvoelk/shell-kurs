e9117e490cfb67cd25c723a2b5b4e5eba3cb340b
tg00021 <tg00021@localhost.localdomain>
2020-02-10 14:21:18 +0100

    Logdateiname enthält jetzt aktuelles Datum

--- backup.sh
+++ backup.sh
@@ -3,13 +3,14 @@
 # Directories und Files definieren
 backupdir="/tmp/backup/"
 logdir="/tmp/backuplog/"
-log="Backup-Log"
-errorlog="Bakup-Fehler"
+current_date=$(date +%F)
+log="$logdir/Backup-Log_$current_date"
+errorlog="$logdir/Bakup-Fehler_$current_date"
 
 # nötige Directories anlegen
 mkdir -p "$backupdir"
 mkdir -p "$logdir"
 
 # backups anfertigen
-cp -av ~ "$backupdir" >> "$logdir/$log" 2>> "$logdir/$errorlog"
-cp -av /etc "$backupdir" >> "$logdir/$log" 2>> "$logdir/$errorlog"
+cp -av ~ "$backupdir" >> "$log" 2>> "$errorlog"
+cp -av /etc "$backupdir" >> "$log" 2>> "$errorlog"
