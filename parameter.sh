dbd169902057f36c82a9444e15962f66f0583d00
tg00021 <tg00021@localhost.localdomain>
2020-02-11 11:58:12 +0100

    Fehler falls kein Parameter

--- parameter.sh
+++ parameter.sh
@@ -2,4 +2,13 @@
 
 param_count=$#
 
-echo "Du hast ${param_count} Parameter eingegeben"
+
+if [ ${param_count} -gt 0 ]; then
+	echo "Du hast ${param_count} Parameter eingegeben"
+	exit 0
+fi
+
+if [ ${param_count} -eq 0 ]; then
+	echo "Du musst mindestens einen Parameter eingeben"
+	exit -1
+fi
